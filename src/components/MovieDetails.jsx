47efd699ba68e90a114a944152d30426f64884b4
Keerthana Prabhakar <keerthanapillai.kp@gmail.com>
2023-08-01 13:33:53 +0200

    frontend

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+const MovieDetails = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [error, setError] = useState(null);
+        const [movie, setMovie] = useState(null);
+        useEffect(() => {
+            axios
+            .get(`${import.meta.env.VITE_SERVER_BASE_URL}/api/movies/${id}`)
+            .then(res => setMovie(res.data))
+            .catch(e => setError(e.response?.data?.message));
+        }, []);
+        const handleDelete = () => {
+            axios
+            .delete(`${import.meta.env.VITE_SERVER_BASE_URL}/api/movies/${id}`)
+            .then(res => navigate('/'))
+            .catch(e => console.log(e));
+        };
+    return (
+    <div>
+        {error && <p style={{"color":"Green"}}>{error}</p>}
+        {movie && (
+            <>
+        <h2>{movie.title}</h2>
+        <p>Director: {movie.director}</p>
+        <p>Year: {movie.year}</p>
+        <Link to={`/movies/${id}/update`}>Update Movie</Link>
+        <button onClick={handleDelete}>Delete Movie</button>
+        </>
+        )}
+</div>
+    );
+};
+
+export default MovieDetails
